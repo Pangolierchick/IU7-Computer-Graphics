c57f2cf711564d92855ba36b32ee3a619f58fe24
Pangolierchick <gogofun@bk.ru>
2021-04-11 22:07:19 +0300

    small fixes

--- lab_04/src/methods/parametric.cpp
+++ lab_04/src/methods/parametric.cpp
@@ -5,8 +5,8 @@ void parametric_circle(drawArea &area, point &c, float r, bool draw) {
     float l = roundf(M_PI * r / 2.0) / 2.0;
 
     for (float i = 0; i <= l; i++) {
-        float x = roundf(r * cosf(i / r));
-        float y = roundf(r * sinf(i / r));
+        float x = r * cosf(i / r);
+        float y = r * sinf(i / r);
 
         if (draw)
             plot_circle(area, c.x, x + c.x, c.y, y + c.y);
@@ -18,8 +18,8 @@ void parametric_ellipse(drawArea &area, point &c, float a, float b, bool draw) {
     float l = roundf(M_PI * m / 2.0f);
 
     for (float i = 0; i <= l; i++) {
-        float x = roundf(a * cosf(i / m));
-        float y = roundf(b * sinf(i / m));
+        float x = a * cosf(i / m);
+        float y = b * sinf(i / m);
 
         if (draw)
             plot_ellipse(area, c.x, x, c.y, y);
